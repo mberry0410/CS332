68cfc9bb6193719257ec82d5897634746fc43c77
mberry0410 <237880170+mberry0410@users.noreply.github.com>
2026-06-15 03:39:13 +0000

    Complete Lab 2 insertion sort

---
+++ lab02/mjbyrd_lab02.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+
+ //Lab 2 
+ //to compile: gcc mjbyrd_lab02.c -o lab02
+ // to Run: ./lab02
+
+int main() {
+    int N;
+
+    //Asks the user for the amount of elements
+    printf("Please enter number of elements in array: ");
+    scanf("%d", &N);
+
+    int arr[N];
+
+    for (int i = 0; i < N; i++) {
+        printf("Please enter element %d of array: ", i + 1);
+        scanf("%d", &arr[i]);
+    }
+
+    // Displays the original array
+    printf("Given array is: [");
+    for (int i = 0; i < N - 1; i++) {
+        printf("%d, ", arr[i]);
+    }
+    printf("%d]\n", arr[N - 1]);
+
+    // Insertion Sort
+    int temp, currLoc;
+    for (int i = 1; i < N; i++) {
+        currLoc = i;
+        while (currLoc > 0 && arr[currLoc - 1] > arr[currLoc]) {
+            temp = arr[currLoc];
+            arr[currLoc] = arr[currLoc - 1];
+            arr[currLoc - 1] = temp;
+            currLoc--;
+        }
+    }
+
+    // Displays the array after sorting
+    printf("Sorted array is: [");
+    for (int i = 0; i < N - 1; i++) {
+        printf("%d, ", arr[i]);
+    }
+    printf("%d]\n", arr[N - 1]);
+
+    return 0;
+}
